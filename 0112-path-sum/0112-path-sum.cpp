b7eff239820f1b3083dbd57fa42200a83e85d35d
Sameer Chore <sameerchore5@gmail.com>
2026-07-08 23:49:21 +0530

    Update 0112-path-sum.cpp

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -1,31 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+//in this wihtout using the global maxi varible we add in function and received by pointers using & so to actual value change of maxi
+
 class Solution {
 public:
-int sum=0;
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        if(root==nullptr){return false; }
-        sum+=root->val;
-        bool l=false;
-        bool r=false;
-        if(root->left){l=hasPathSum(root->left,targetSum);}
-        if(l==true)return true;
+    int maxPathSum(TreeNode* root) {
+        int res = root->val;
+        dfs(root, res);
+        return res;
+    }
 
-        if(root->right){r=hasPathSum(root->right,targetSum);}
-        if(l==true)return true;
+private:
+    int dfs(TreeNode* node, int& res) {
+        if (!node) {
+            return 0;
+        }
 
-        bool leafNode=(!root->left && !root->right)? true:false;
-        if(sum==targetSum && leafNode )return true;
-        else{sum-=root->val;}
-        return l||r;
+        // Recursively compute the maximum sum of the left and right subtree paths.
+        int leftSum = max(0, dfs(node->left, res));
+        int rightSum = max(0, dfs(node->right, res));
+
+        // Update the maximum path sum encountered so far (with split).
+        res = max(res, leftSum + rightSum + node->val);
+
+        // Return the maximum sum of the path (without split).
+        return max(leftSum, rightSum) + node->val;
     }
 };
