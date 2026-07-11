e9f2863ae7d999c796f0bd855e8a33d25e132135
Sameer Chore <sameerchore5@gmail.com>
2026-07-11 23:10:46 +0530

    Update 0222-count-complete-tree-nodes.cpp

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -10,13 +10,25 @@
  * };
  */
 class Solution {
+
 public:
-    int cnt=0;
+
     int countNodes(TreeNode* root) {
-      if(root==nullptr)return cnt;
-       countNodes(root->left); 
-       countNodes(root->right);
-       cnt++;
-       return cnt;
+
+        if(!root) return 0;
+
+        int hl=0, hr=0;
+
+        TreeNode *l=root, *r=root;
+
+        while(l) {hl++;l=l->left;}
+
+        while(r) {hr++;r=r->right;}
+
+        if(hl==hr) return pow(2,hl)-1;
+
+        return 1+countNodes(root->left)+countNodes(root->right);
+
     }
+
 };
