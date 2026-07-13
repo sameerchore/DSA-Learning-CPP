7440f1a001b88fbc35007bd83dea342b05c78e12
Sameer Chore <sameerchore5@gmail.com>
2026-07-13 19:59:21 +0530

    Update 0103-binary-tree-zigzag-level-order-traversal.cpp

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,29 +12,58 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>res;
-        if(root==nullptr)return res;
-        res.push_back({root->val});
-        if(!root->right && !root->left)return res;
-        queue<TreeNode*>qt;
-        qt.push(root);
-        bool fg=false;
-        while(!qt.empty()){
-            vector<int>ans={};
-            int size=qt.size();
-            for(int i=0;i<size;i++){
-            TreeNode* node=qt.front();
-            qt.pop();
-               if(node->left){qt.push(node->left);ans.push_back(node->left->val);}
-               if(node->right){qt.push(node->right);ans.push_back(node->right->val);} 
-            }
-            if(!ans.empty()){
-            fg=!fg;
-            if(fg){reverse(ans.begin(),ans.end());}
-            res.push_back(ans);
+        // Vector to store the result of zigzag traversal
+        vector<vector<int>> result;
+
+        // Check if the root is NULL, return an empty result
+        if (root == nullptr) {
+            return result;
+        }
+
+        // Queue to perform level order traversal
+        queue<TreeNode*> nodesQueue;
+        nodesQueue.push(root);
+
+        // Flag to determine the direction of traversal (left to right or right to left)
+        bool leftToRight = true;
+
+        // Continue traversal until the queue is empty
+        while (!nodesQueue.empty()) {
+            // Get the number of nodes at the current level
+            int size = nodesQueue.size();
+
+            // Vector to store the values of nodes at the current level
+            vector<int> row(size);
+
+            // Traverse nodes at the current level
+            for (int i = 0; i < size; i++) {
+                // Get the front node from the queue
+                TreeNode* node = nodesQueue.front();
+                nodesQueue.pop();
+
+                // Determine the index to insert the node's value based on the traversal direction
+                int index = leftToRight ? i : (size - 1 - i);
+
+                // Insert the node's value at the determined index
+                row[index] = node->data;
+
+                // Enqueue the left and right children if they exist
+                if (node->left) {
+                    nodesQueue.push(node->left);
+                }
+                if (node->right) {
+                    nodesQueue.push(node->right);
+                }
             }
 
+            // Switch the traversal direction for the next level
+            leftToRight = !leftToRight;
+
+            // Add the current level's values to the result vector
+            result.push_back(row);
         }
-        return res;
+
+        // Return the final result of zigzag level order traversal
+        return result;
     }
 };
