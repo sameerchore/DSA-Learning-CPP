2498b2a97eec719b14ce762e877c44ed8460af93
Sameer Chore <sameerchore5@gmail.com>
2026-07-21 18:04:30 +0530

    Implement optimal DFS for binary tree right side view
    Added an optimal DFS solution for the right side view of a binary tree.

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -31,3 +31,26 @@ public:
     return rightView;
     }
 };
+
+
+//dfs solution optimlal:
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> view;
+        rightView(root, view, 0);
+        return view;
+    }
+private:
+    void rightView(TreeNode* root, vector<int>& view, int level) {
+        if (!root) {
+            return;
+        }
+        if (view.size() == level) {
+            view.push_back(root -> val);
+        }
+        rightView(root -> right, view, level + 1);
+        rightView(root -> left, view, level + 1);
+    }
+};
+explain this one
