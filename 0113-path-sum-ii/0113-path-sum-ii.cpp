4684650d9a0b9938d0135427464c55da2de930d0
Sameer Chore <sameerchore5@gmail.com>
2026-07-28 22:01:18 +0530

    Time: 13 ms (22.72%), Space: 40.4 MB (15.49%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -1,4 +1,3 @@
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -34,68 +33,3 @@ public:
         pathDFS(root->right,target,tot,ans,sum);
     }
 };
-
-
-
-//Optimal Solution 
-void dfs(...) {
-    // Make a choice
-    path.push_back(x);
-
-    dfs(...);
-    dfs(...);
-
-    // Undo the choice
-    path.pop_back();
-}
-// THis is the flow of the ans for path sum 
-// here we pop back becoz apn ata leaf vr ah tri trgetsum achive nh zhl so pop ani return kru
-
-
-
-//Another approch is by subtracting fform targetsum and if targetsum==0 push the patht to ans vector
-//global vector used and pop used to trace back
-class Solution {
-public:
-    vector<vector<int>> ans;
-    vector<int> cur;
-
-    void dfs(TreeNode* root, int sum) {
-        if (!root) return;
-
-        cur.push_back(root->val);
-        sum -= root->val;
-
-        if (!root->left && !root->right && sum == 0)
-            ans.push_back(cur);
-
-        dfs(root->left, sum);
-        dfs(root->right, sum);
-
-        cur.pop_back();
-    }
-
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        dfs(root, targetSum);
-        return ans;
-    }
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
