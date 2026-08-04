70a91ec720c8be291e2e799338e14a0323772a9a
Sameer Chore <sameerchore5@gmail.com>
2026-08-04 19:44:03 +0530

    gfg-amozon-easy

---
+++ 700161-delete-node-without-linked-list-head-GFG/700161-delete-node-without-linked-list-head.cpp
@@ -0,0 +1,23 @@
+/* Structure of Linked List Node
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+*/
+class Solution {
+  public:
+    void deleteNode(Node* x) {
+        x->data=x->next->data;
+        x->next=x->next->next;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
