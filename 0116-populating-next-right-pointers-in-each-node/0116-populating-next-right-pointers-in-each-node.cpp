f6c0e7af91e44d7cc089f0928ad110edbb810d77
Sameer Chore <sameerchore5@gmail.com>
2026-08-08 00:48:39 +0530

    Revision needed for this BST-populating next ptr
    Implement optimal solution for connecting next right pointers in each node using O(1) space.

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -47,6 +47,36 @@ public:
     }
 };
 
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+
+//Optimal iwht space com:; O(1) by traversing next pointer
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (!root) return nullptr;
+
+        Node* leftmost = root;
+
+        while (leftmost->left) {
+
+            Node* curr = leftmost;
+
+            while (curr) {
+
+                // Connect left child to right child
+                curr->left->next = curr->right;
+
+                // Connect right child to next node's left child
+                if (curr->next) {
+                    curr->right->next = curr->next->left;
+                }
+
+                curr = curr->next;
+            }
+
+            // Move to next level
+            leftmost = leftmost->left;
+        }
+
+        return root;
+    }
+};
