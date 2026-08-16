135041e51fd62b487c9d3544109e024ebee1eff9
Sameer Chore <sameerchore5@gmail.com>
2026-08-16 21:59:00 +0530

    optimal written esy-mid

--- 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
+++ 3159-find-occurrences-of-an-element-in-an-array/3159-find-occurrences-of-an-element-in-an-array.cpp
@@ -20,6 +20,31 @@ public:
     }
 };
 
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
+//Optimal appr:
+//Use vector instead of map.
+
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        vector<int> occurrences;
+        vector<int> ans;
+
+        // Store indices where x occurs
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == x) {
+                occurrences.push_back(i);
+            }
+        }
+
+        // Answer each query
+        for (int q : queries) {
+            if (q <= occurrences.size()) {
+                ans.push_back(occurrences[q - 1]);
+            } else {
+                ans.push_back(-1);
+            }
+        }
+
+        return ans;
+    }
+};
