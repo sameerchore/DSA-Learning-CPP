ec9fe0696a4f0a1cdaff03ece969cec5dee737fe
Sameer Chore <sameerchore5@gmail.com>
2026-09-08 15:57:20 +0530

    need revision on this

--- 3870-count-commas-in-range/3870-count-commas-in-range.cpp
+++ 3870-count-commas-in-range/3870-count-commas-in-range.cpp
@@ -1,19 +1,7 @@
 class Solution {
 public:
     int countCommas(int n) {
-
-        int commaCnt = 0;
-        for (int i = 1000; i <= n; i++) {
-            int cnt = log10(abs(i)) + 1;
-            if (cnt % 3 == 0 && cnt > 3) {
-                commaCnt += cnt / 3;
-                commaCnt--;
-
-            } else {
-                commaCnt += cnt / 3;
-            }
-        }
-        return commaCnt;
+        return (n>999)? n-999:0;
     }
 };
 
