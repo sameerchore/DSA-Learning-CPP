363f9888f943736aef310df57b03a65ddb79a4d6
Sameer Chore <sameerchore5@gmail.com>
2026-09-25 01:44:53 +0530

    mussstttt neededddd practice in recursion

---
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        int sum=0;
+        dfs(root,sum);
+        return root;
+    }
+    int dfs(TreeNode* root,int&sum){
+        if(!root)return 0;
+        dfs(root->right,sum);
+        sum=sum+root->val;
+        root->val=sum;
+        dfs(root->left,sum);
+        return sum;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
